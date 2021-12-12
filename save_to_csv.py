30ac4466b711d28e587c5cf8a375b62fac4e7dd8
DaeLee <48057905+GisangLee@users.noreply.github.com>
2021-12-12 19:37:30 +0900

    finish

--- save_to_csv.py
+++ save_to_csv.py
@@ -2,4 +2,13 @@ import csv
 
 
 def save_file_to_csv(jobs_data):
-    print
+    file = open("jobs.csv", mode="w", encoding="utf-8")
+    writer = csv.writer(file)
+
+    row_title = ["job_title", "company_name", "location", "apply_link"]
+
+    writer.writerow(row_title)
+
+    for job in jobs_data:
+        writer.writerow(list(job.values()))
+    return 0
